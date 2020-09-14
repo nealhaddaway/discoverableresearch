48871fb82a7cbfdbe0cee363fbd42d64ebaa2758
Neal Haddaway <nealhaddaway@Neals-iMac.fritz.box>
2020-09-14 13:42:59 +0200

    Update NAMESPACE

--- R/sample_titles.R
+++
@@ -1,9 +0,0 @@
-#' Sample set of 10,000 records from PubMed
-#'
-#' A dataset containing a sample set of records returned in PubMed in Sep 2020 using a search for 'research' in 
-#' the basic search facility and exporting the first 10,000 records based on publication date.
-#'
-#' @format An .ris reference file containing 10,000 records.
-#' }
-#' @source \url{https://pubmed.ncbi.nlm.nih.gov/}
-"sample_titles"
