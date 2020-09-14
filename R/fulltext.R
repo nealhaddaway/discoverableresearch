344bf9cfc5609549ba0917875343970dc370c49c
Neal Haddaway <nealhaddaway@Neals-iMac.fritz.box>
2020-09-14 11:02:01 +0200

    Data upload and description

---
+++ R/fulltext.R
@@ -0,0 +1,8 @@
+#' Sample full text
+#'
+#' An example full text document.
+#'
+#' @format A text file of a full text research article.
+#' }
+#' @source \url{https://link.springer.com/article/10.1007/s13280-016-0773-x}
+"fulltext"
